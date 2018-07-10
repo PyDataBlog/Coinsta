198c1e5f7181228f68fcb1b4579d50e4ceb61778
Bernard Brenyah <pimpfada@gmail.com>
2018-07-10 18:51:43 +0200

    reinstated hard coding of __version__

--- coinsta/__init__.py
+++ coinsta/__init__.py
@@ -3,8 +3,7 @@
 
 from .core import Historical, Current
 from .exceptions import WrongCoinCode
-from pkg_resources import get_distribution
 
-__version__ = get_distribution('coinsta').version
+__version__ = '0.1.2'
 __author__ = 'Bernard Brenyah <bbrenyah@gmail.com>'
 __all__ = []
